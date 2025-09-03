2f25a9d51bd5e36073175620ff928a9a46a702af
nikolaos83 <54727787+nikolaos83@users.noreply.github.com>
2025-09-03 19:42:54 +0300

    Update firewall-hardened.sh

--- firewall-hardened.sh
+++ firewall-hardened.sh
@@ -28,7 +28,8 @@ ip6tables -A INPUT -p udp --dport 41641 -j ACCEPT
 ip6tables -A INPUT -p tcp --dport 22 -j ACCEPT
 
 # Persistence
-mkdir -p /etc/iptables
+apt install iptables-persistent -y
+#mkdir -p /etc/iptables
 iptables-save > /etc/iptables/rules.v4
 ip6tables-save > /etc/iptables/rules.v6
 
